580fc3fdfd725d0007b19a9bbbaeac02675eef33
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-04-05 22:41:48 -0700

    Note Modal, Basic Open/Close State

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+// React
+import { useState } from "react";
+
+// Material UI
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 // CSS
 import "./css/App.css";
 
@@ -6,12 +12,29 @@ import SearchBar from "./components/SearchBar";
 import NotesProgressBar from "./components/NotesProgressBar";
 import NotesNavigation from "./components/NotesNavigation";
 import NotesBoard from "./components/NotesBoard";
+import NoteModal from "./components/NoteModal";
 
 function App() {
+    // Modal State
+    const [open, setOpen] = useState(false);
+
+    // Modal Handlers
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <div className="App">
+            <CssBaseline />
+            <NoteModal open={open} handleClose={handleClose} />
             <SearchBar />
             <NotesNavigation />
+            <button type="button" onClick={handleOpen}>
+                Open Modal
+            </button>
             <NotesProgressBar />
             <NotesBoard />
         </div>
--- src/components/NoteModal.tsx
+++ src/components/NoteModal.tsx
@@ -1,5 +1,27 @@
-const NoteModal = () => {
-    return <h1>Note Modal</h1>;
+// Material UI
+import Modal from "@material-ui/core/Modal";
+import { styled } from "@material-ui/core/styles";
+
+// CSS
+import "../css/Modal.css";
+
+const NoteModalComponent = styled(Modal)({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+});
+
+type ModalProp = {
+    open: boolean;
+    handleClose: () => void;
+};
+
+const NoteModal = ({ open, handleClose }: ModalProp) => {
+    return (
+        <NoteModalComponent open={open} onClose={handleClose}>
+            <div className="noteContainer">Note Modal</div>
+        </NoteModalComponent>
+    );
 };
 
 export default NoteModal;
