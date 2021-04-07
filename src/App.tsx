4699cc8389b016596293b2755bd417348d17059a
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-04-07 15:27:54 -0700

    Menu Styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,32 @@ import { useState } from "react";
 
 // Material UI
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Button from "@material-ui/core/Button";
+
 // CSS
 import "./css/App.css";
 
 // Components
 import SearchBar from "./components/SearchBar";
-import NotesProgressBar from "./components/NotesProgressBar";
-import NotesNavigation from "./components/NotesNavigation";
-import NotesBoard from "./components/NotesBoard";
 import NoteModal from "./components/NoteModal";
+import NotesNavigation from "./components/NotesNavigation";
 
 function App() {
     // Modal State
     const [open, setOpen] = useState(false);
 
     // Modal Handlers
-    const handleOpen = () => {
-        setOpen(true);
-    };
     const handleClose = () => {
         setOpen(false);
     };
-
+    const handleOpen = () => {
+        setOpen(true);
+    };
     return (
         <div className="App">
             <CssBaseline />
             <NoteModal open={open} handleClose={handleClose} />
             <SearchBar />
-            <div>
-                <NotesNavigation />
-                <Button onClick={handleOpen}>+ Add Note</Button>
-            </div>
-            <NotesProgressBar />
-            <NotesBoard />
+            <NotesNavigation handleOpen={handleOpen} />
         </div>
     );
 }
--- src/components/NotesNavigation.tsx
+++ src/components/NotesNavigation.tsx
@@ -1,19 +1,84 @@
+// React
+import { useState } from "react";
+
 // Material UI
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
+import TabPanel from "@material-ui/lab/TabPanel";
+import TabList from "@material-ui/lab/TabList";
+import TabContext from "@material-ui/lab/TabContext";
 import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import { styled } from "@material-ui/core/styles";
 
-const NotesNavigation = () => {
+// Components
+import NotesProgressBar from "./NotesProgressBar";
+import NotesBoard from "./NotesBoard";
+
+type NavProp = {
+    handleOpen: () => void;
+};
+
+const NavContainer = styled(Container)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    maxWidth: "65%"
+});
+const CustomAppBar = styled(AppBar)({
+    maxWidth: "60%",
+    background: "none",
+    color: "black",
+    boxShadow: "none"
+});
+const AddNotesButton = styled(Button)({
+    background: "#2196F3",
+    color: "white",
+    padding: "1em 5em",
+    whiteSpace: "nowrap"
+});
+
+const NotesNavigation = ({ handleOpen }: NavProp) => {
+    const [tab, setTab] = useState("all");
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+        setTab(newValue);
+    };
     return (
         <div>
-            <AppBar position="static">
-                <Tabs>
-                    <Tab label="All" />
-                    <Tab label="Home" />
-                    <Tab label="Work" />
-                    <Tab label="Personal" />
-                </Tabs>
-            </AppBar>
+            <TabContext value={tab}>
+                <NavContainer>
+                    <CustomAppBar position="static">
+                        <TabList onChange={handleChange}>
+                            <Tabs>
+                                <Tab label="All" value={"all"} />
+                                <Tab label="Home" value={"home"} />
+                                <Tab label="Work" value={"work"} />
+                                <Tab label="Personal" value={"personal"} />
+                            </Tabs>
+                        </TabList>
+                    </CustomAppBar>
+                    <AddNotesButton onClick={handleOpen}>
+                        + Add Note
+                    </AddNotesButton>
+                </NavContainer>
+                <TabPanel value="all">
+                    <NotesProgressBar />
+                    <NotesBoard />
+                </TabPanel>
+                <TabPanel value="home">
+                    <NotesProgressBar />
+                    <NotesBoard />
+                </TabPanel>
+                <TabPanel value="work">
+                    <NotesProgressBar />
+                    <NotesBoard />
+                </TabPanel>
+                <TabPanel value="personal">
+                    <NotesProgressBar />
+                    <NotesBoard />
+                </TabPanel>
+            </TabContext>
         </div>
     );
 };
